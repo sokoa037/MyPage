fddeefa28d1b26de6dc75b6e584b94af90d3c383
sokoa037 <sokoa037@osloskolen.no>
2026-08-28 10:40:06 +0200

    click game

---
+++ script.js
@@ -0,0 +1,19 @@
+function myfunction(){
+    var element=document.body;
+    element.classList.toggle("dark-mode")
+}
+
+
+
+
+
+   let poeng = 0;
+   
+   const hello = document.getElementById("tekst");
+   const button = document.getElementById("knapp");
+   const knapp3 = document.getElementById("reset");
+
+  function leggTil (){
+    poeng = poeng + 1;
+    document
+  }
